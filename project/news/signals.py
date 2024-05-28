360739e53bfb5081f33f66aab4a50a36450c885f
podbor16 <podbor250718@gmail.com>
2024-05-28 12:50:10 +0700

    Working version with signal for created post at category

--- project/news/signals.py
+++ project/news/signals.py
@@ -1,35 +1,34 @@
 from django.contrib.auth.models import User
 from django.core.mail import EmailMultiAlternatives
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from django.template.defaultfilters import truncatewords
 
-from .models import Post
+from .models import Post, PostCategory
 
 
-@receiver(post_save, sender=Post)
-def product_created(instance, created, **kwargs):
-    if not created:
-        return
+@receiver(m2m_changed, sender=PostCategory)
+def notify_subscribers(instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.category.all()
+        emails = User.objects.filter(
+            subscriptions__category__in=categories
+        ).values_list('email', flat=True)
 
-    emails = User.objects.filter(
-        subscriptions__category=instance.category
-    ).values_list('email', flat=True)
+        subject = f'Новая публикация в категориях {", ".join(category.__str__() for category in categories)}'
 
-    subject = f'Новая публикация в категории {instance.category}'
-
-    text_content = (
-        f'Заголовок: {instance.title}\n'
-        f'Краткое содержание: {truncatewords(instance.text, 5)}\n\n'
-        f'Ссылка на товар: http://127.0.0.1:8000{instance.get_absolute_url()}'
-    )
-    html_content = (
-        f'Заголовок: {instance.title}\n'
-        f'Краткое содержание: {truncatewords(instance.text, 5)}\n\n'
-        f'<a href="http://127.0.0.1:8000{instance.get_absolute_url()}">'
-        f'Ссылка на товар</a>'
-    )
-    for email in emails:
-        msg = EmailMultiAlternatives(subject, text_content, None, [email])
-        msg.attach_alternative(html_content, "text/html")
-        msg.send()
+        text_content = (
+            f'Заголовок: {instance.title}\n'
+            f'Краткое содержание: {truncatewords(instance.text, 5)}\n\n'
+            f'Ссылка на товар: http://127.0.0.1:8000{instance.get_absolute_url()}'
+        )
+        html_content = (
+            f'Заголовок: {instance.title}<br>'
+            f'Краткое содержание: {truncatewords(instance.text, 5)}<br><br>'
+            f'<a href="http://127.0.0.1:8000{instance.get_absolute_url()}">'
+            f'Ссылка на товар</a>'
+        )
+        for email in emails:
+            msg = EmailMultiAlternatives(subject, text_content, None, [email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
--- project/project/settings.py
+++ project/project/settings.py
@@ -171,7 +171,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "django-example@yandex.ru"
