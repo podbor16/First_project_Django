b9df410117dfaee7447b3a40ffc7cc0710efcdc0
podbor16 <podbor250718@gmail.com>
2024-07-18 13:58:46 +0700

    added a custom logging for the final task 13.4

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import logging
+import logging.config
+from django.utils.log import AdminEmailHandler
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +37,6 @@ ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = '/posts'
 LOGOUT_REDIRECT_URL = '/posts'
-
 
 
 # Application definition
@@ -208,3 +210,141 @@ CACHES = {
         'TIMEOUT': 30,
     }
 }
+
+
+class DebugTrueFilter(logging.Filter):
+    def filter(self, record):
+        return DEBUG
+
+
+class DebugFalseFilter(logging.Filter):
+    def filter(self, record):
+        return not DEBUG
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'style': '{',
+    'formatters': {
+        'console_debug': {
+            'format': '%(asctime)s %(levelname)s %(message)s'
+        },
+        'console_warning': {
+            'format': '%(asctime)s %(levelname)s %(message)s %(pathname)s'
+        },
+        'console_error': {
+            'format': '%(asctime)s %(levelname)s %(message)s %(pathname)s %(exc_info)s'
+        },
+        'file_general': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(message)s',
+        },
+        'file_error': {
+            'format': '%(asctime)s %(levelname)s %(message)s %(pathname)s %(exc_info)s',
+        },
+        'file_security': {
+            'format': '%(aasctime)s %(levelname)s %(module)s %(message)s'
+        },
+        'mail_admins': {
+            'format': '%(asctime)s %(levelname)s %(message)s %(pathname)s',
+        }
+    },
+    'filters': {
+        'debug_true': {
+            '()': DebugTrueFilter,
+        },
+        'require_debug_false': {
+            '()': DebugFalseFilter,
+        }
+    },
+    'handlers': {
+        'console_debug': {
+            'level': 'DEBUG',
+            'filters': ['debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_debug'
+        },
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_warning'
+        },
+        'console_error': {
+            'level': 'ERROR',
+            'filters': ['debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_error'
+        },
+        'file_general': {
+            'level': 'INFO',
+            'filters': ['debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'file_general'
+        },
+        'file_error': {
+            'level': 'ERROR',
+            #'filters': ['debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': 'errors.log',
+            'formatter': 'file_error'
+        },
+        'file_security': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'security.log',
+            'formatter': 'file_security'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': 'debug_false',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'mail_admins',
+            'include_html': True
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console_debug', 'console_warning', 'console_error', 'file_general'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.template': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django-security': {
+            'handlers': ['file_security'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    }
+}
+
+
+# Обработка стэка ошибок для сообщений ERROR и CRITICAL в консоли
+class ConsoleErrorHandler(logging.StreamHandler):
+    def emit(self, record):
+        if record.levelno >= logging.ERROR:
+            record.exc_info = True
+        super().emit(record)
+
+
+LOGGING['handlers']['console_error']['class'] = 'project.settings.ConsoleErrorHandler'
